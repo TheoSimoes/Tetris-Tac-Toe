e24b4ecef812d9d50dff6f443a80bf2e53465780
TheoSimoes <43986966+TheoSimoes@users.noreply.github.com>
2024-03-19 16:38:31 +0100

    Update TetriminoGrid.cs

--- Assets/Scripts/Gameplay/TetriminoGrid.cs
+++ Assets/Scripts/Gameplay/TetriminoGrid.cs
@@ -1,5 +1,7 @@
+using System;
 using UnityEngine;
 using UnityEngine.Tilemaps;
+using Utils;
 
 namespace Gameplay
 {
@@ -15,7 +17,7 @@ namespace Gameplay
         [SerializeField]
         private TileBase[] _groundTiles;
 
-        [ContextMenu("Generate TetriminoGrid")]
+/*        [ContextMenu("Generate TetriminoGrid")]
         public void Generate()
         {
             var cave = new Cave(width, height);
@@ -26,12 +28,46 @@ namespace Gameplay
                 var ground = _groundTiles[0];
                 _groundTilemap.SetTile(new Vector3Int(cell.X, cell.Y, 0), ground);
             }
-        }
+        }*/
+
         [ContextMenu("Generate Tetrimino")]
         public void ColorGrid()
         {
-            Debug.Log(string.Format(Tetrimino.GetRandomTetrimino().Name));
+            Cave cave = new Cave(width, height);
+            _groundTilemap.ClearAllTiles();
 
+            Tetrimino tetrimino = Tetrimino.GetRandomTetrimino();
+            Debug.Log(string.Format(tetrimino.Name));
+            TileBase ground = _groundTiles[0];
+            switch (tetrimino.Name)
+            {
+                case "Square":
+                    ground = _groundTiles[1];
+                    break;
+                case "Line":
+                    ground = _groundTiles[2];
+                    break;
+                case "T":
+                    ground = _groundTiles[3];
+                    break;
+                case "Z":
+                    ground = _groundTiles[4];
+                    break;
+                case "S":
+                    ground = _groundTiles[5];
+                    break;
+                case "L":
+                    ground = _groundTiles[6];
+                    break;
+                case "J":
+                    ground = _groundTiles[7];
+                    break;
+            }
+            foreach (var cell in cave.Grid.Cells)
+            {
+
+                _groundTilemap.SetTile(new Vector3Int(cell.X, cell.Y, 0), ground);
+            }
         }
     }
 }
